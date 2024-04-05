23efbee1afe5b769535d8634ef134a35618882b4
theka <thekadude55@gmail.com>
2024-04-04 22:59:27 -0700

    tried to implement moves timing

--- src/Backend/Board.java
+++ src/Backend/Board.java
@@ -8,28 +8,21 @@ import java.util.TimerTask;
 
 public class Board extends TimerTask{
     private int time;
-    private static int[][] board;
-    public Board() {
+    private int[][] board;
+    public Board(int boardsize) {
         time = 0;
         Timer clock = new Timer();
+        board = new int[boardsize][boardsize];
         clock.scheduleAtFixedRate(this, 0, 2000); // Schedule the current instance
     }
-
     @Override
     public void run() {
         time++;
         System.out.println(time);
+        foodGen(board);
         arrprint(board);
     }
-    public static HashMap<String,Integer> inputaker() throws IOException{
-        HashMap<String,Integer> map = new HashMap<>();
-        InputStreamReader in = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(in);
-        System.out.println("Set Board Size: ");
-        int boardsize = Integer.parseInt(br.readLine());
-        map.put("boardsize",boardsize);
-        return map;
-    }
+
     public static void arrprint(int[][] board) {
         for (int i = 0; i < board.length; i++) {
             System.out.println(Arrays.toString(board[i]));
@@ -45,19 +38,28 @@ public class Board extends TimerTask{
         }
         return board;
     }
-    public static void foodGen(int boardsize){
-//        int randomInt1 = random.nextInt(boardsize)-1;
-//        int randomInt2 = random.nextInt(boardsize)-1;
-//        board[randomInt1][randomInt2] = 1;
-    }
-    public void actionPerformed (ActionEvent e) {
-        time++;
-
+    public static void foodGen(int[][] board){
+        Random random = new Random();
+        int randomInt1;
+        int randomInt2;
+        do {
+            randomInt1 = random.nextInt(board.length);
+            randomInt2 = random.nextInt(board.length);
+        } while (board[randomInt1][randomInt2] == 1);
+        board[randomInt1][randomInt2] = 1;
     }
     public static void main(String[]args) throws IOException {
         HashMap<String,Integer> map = inputaker();
         int boardsize = map.get("boardsize");
-        board = board(boardsize);
-        arrprint(board);
+        Board gameBoard = new Board(boardsize);
+    }
+    public static HashMap<String,Integer> inputaker() throws IOException{
+        HashMap<String,Integer> map = new HashMap<>();
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+        System.out.println("Set Board Size: ");
+        int boardsize = Integer.parseInt(br.readLine());
+        map.put("boardsize",boardsize);
+        return map;
     }
 }
