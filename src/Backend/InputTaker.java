a82a9087d03927dedbbe6343124b2788bb6e6b38
vglalala <stingzhang9000@gmail.com>
2024-04-09 10:20:26 -0700

    update

--- src/Backend/InputTaker.java
+++ src/Backend/InputTaker.java
@@ -2,6 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+
+
 
 public class InputTaker {
     public static HashMap<String,Integer> inputaker() throws IOException {
@@ -13,4 +24,28 @@ public class InputTaker {
         map.put("boardsize",boardsize);
         return map;
     }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
+
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
+                break;
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
+                break;
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        new KeyListenerExample();
+    }
+
+
 }
