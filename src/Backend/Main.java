25d0130deadbb780141cf80bd47c23411fa3b77a
theka <thekadude55@gmail.com>
2024-04-09 22:03:25 -0700

    update

--- src/Backend/Main.java
+++ src/Backend/Main.java
@@ -1,31 +1,69 @@
 package Backend;
+import javax.swing.*;
+import java.awt.*;
 import java.util.*;
 import java.io.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Timer;
 import java.util.TimerTask;
-public class Main extends TimerTask {
+public class Main extends TimerTask{
+    private JFrame frame;
     private int time;
+    public static int[][] board;
     public static int boardSize = InputTaker.sizeGetter();
-    private int[][] board;
-
-    public Main(int boardsize) {
+    public static JLabel[][] frames = Main.createLabelGrid(boardSize, boardSize);
+    public Main() {
         board = Board.board(boardSize);
+        frame = new JFrame();
+        frame.setTitle("BattleSnake");
+        frame.setSize(1000,1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        JPanel panel = new JPanel(new GridLayout(boardSize,boardSize));
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                JLabel label = new JLabel(""+board[i][j]);
+                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                label.setFont(new Font("Serif", Font.PLAIN, 35));
+                label.setHorizontalAlignment(JLabel.CENTER);
+                label.setVerticalAlignment(JLabel.CENTER);
+                panel.add(label);
+                frames[i][j] = label;
+            }
+        }
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
         time = 0;
         Timer clock = new Timer();
         clock.scheduleAtFixedRate(this, 0, 2000);
     }
-
+    private static JLabel[][] createLabelGrid(int rows, int cols) {
+        JLabel[][] labels = new JLabel[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                labels[i][j] = new JLabel(); // Initialize each JLabel
+            }
+        }
+        return labels;
+    }
+    public static int[][] getBoard() {
+        return board;
+    }
     @Override
     public void run() {
         time++;
+        for (int i = 0; i < frames.length; i++) {
+            for (int j = 0; j < frames.length; j++) {
+                frames[i][j].setText(Integer.toString(board[i][j]));
+            }
+        }
         System.out.println(time);
         Board.foodGen(board);
         Board.arrprint(board);
     }
 
     public static void main(String[] args) {
-        Main game = new Main(boardSize);
+        Main backendMain = new Main();
     }
 }
