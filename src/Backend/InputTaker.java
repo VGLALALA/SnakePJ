f01675f49c65db18252b4cb3f08a12b564f47373
theka <thekadude55@gmail.com>
2024-04-09 20:56:50 -0700

    update

--- src/Backend/InputTaker.java
+++ src/Backend/InputTaker.java
@@ -2,7 +2,7 @@ package Backend;
 import java.util.Scanner;
 
 public class InputTaker {
-    public static int sizeGetter() {
+        public static int sizeGetter() {
         Scanner myObj = new Scanner(System.in);
         System.out.println("What board size?: ");
         int size = myObj.nextInt();
