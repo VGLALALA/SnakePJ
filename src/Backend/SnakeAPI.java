2197a65db2a436aeb9ec1eade7247e771957793a
vglalala <stingzhang9000@gmail.com>
2024-04-11 14:40:28 -0700

    move Main class to Frontend folder

--- src/Backend/SnakeAPI.java
+++ src/Backend/SnakeAPI.java
@@ -6,21 +6,32 @@ public class SnakeAPI {
     private static int[][] snake = [[1,1]];
     public static int[][] addX(int[][] myArray[], int[] x) {
         int i;
-        List arrList = new ArrayList( Arrays.asList(myArray));
+        List arrList = new ArrayList(Arrays.asList(myArray));
         arrList.add(x);
         myArray = arrList.toArray(myArray);
         return myArray;
     }
-    public static int[][] doMove(int[][] board,int key,boolean person) {
+    public static int[][] doMove(int[][] board,String key,boolean person) {
 
         if (person){
-            if(key == 87){
+            key = Integer.parseInt(key);
+            if(key == 87){ //up
                 snake = addX(snake, [snake[0],snake[1]+1]);
-            } else if (key == 83) {
+            } else if (key == 83) { // down
                 snake = addX(snake, [snake[0],snake[1]-1]);
-            } else if(key == 68){
+            } else if(key == 68){ // right
                 snake = addX(snake, [snake[0]+1,snake[1]]);
-            } else if (key == 65) {
+            } else if (key == 65) { // left
+                snake = addX(snake, [snake[0]-1,snake[1]]);
+            }
+        } else{
+            if(key == "UP"){ //up
+                snake = addX(snake, [snake[0],snake[1]+1]);
+            } else if (key == "DOWN") { // down
+                snake = addX(snake, [snake[0],snake[1]-1]);
+            } else if(key == "RIGHT"){ // right
+                snake = addX(snake, [snake[0]+1,snake[1]]);
+            } else if (key == "LEFT") { // left
                 snake = addX(snake, [snake[0]-1,snake[1]]);
             }
         }
