dac0df85acc5c8816a2070438b971f1d7818c835
theka <thekadude55@gmail.com>
2024-04-15 13:07:46 -0700

    update

--- src/Backend/SnakeAPI.java
+++ src/Backend/SnakeAPI.java
@@ -3,32 +3,23 @@ import java.lang.*;
 import java.util.*;
 
 public class SnakeAPI {
-    public static int[][] snake;
-    public static int[][] snake2 = {{1,1}};
-    public static int health = 100;
-    public static int health2 = 100;
-    public static int snakelength = snake.length;
-    public static int snakelength2 = snake2.length;
-    public DataWrapper getValue(String condition) {
-        if (condition == "HEALTH") {
-            return new DataWrapper(health,health2);
-        } else if(condition == "LENGTH") {
-            return new DataWrapper(snakelength,snakelength2);
-        } else if (condition == "BODY") {
-            return new DataWrapper(snake,snake2);
-        }
-    }
+    private static int[][] snake  = {{0 , 0}};;
+    private static int[][] snake2 = {{1,1}};
+    private static int health = 100;
+    private static int health2 = 100;
+//    private static int snakelength = snake.length;
+//    private static int snakelength2 = snake2.length;
     public static int[][] addX(int[][] myArray, int[] x) {
         List<int[]> arrList = new ArrayList<>(Arrays.asList(myArray));
         arrList.add(x);
         return arrList.toArray(new int[arrList.size()][2]);
     }
-    public static int[][] removeBack(int[][] array) {
+    public static int[][] removeFront(int[][] array) {
         if (array.length <= 1) {
-            return new int[0][0]; // If the array has only one or zero elements, return an empty array
+            return new int[0][0];
         } else {
             int[][] result = new int[array.length - 1][];
-            System.arraycopy(array, 0, result, 0, array.length - 1);
+            System.arraycopy(array, 1, result, 0, array.length - 1);
             return result;
         }
     }
@@ -45,12 +36,11 @@ public class SnakeAPI {
     public static int[][] doMove(int[][] board,int key,boolean person) {
         int[] head;
         int[] head2;
-        int[][] snakesnake  = {{board.length - 2 , 0}};
+        head = snake[snake.length-1];
         if (person) {
-            head = snake[snake.length-1];
+
             if (key == 87) {
                 snake = addX(snake, new int[]{head[0] - 1, head[1]});
-
             } else if (key == 83) {
                 snake = addX(snake, new int[]{head[0] + 1, head[1]});
 
@@ -71,8 +61,10 @@ public class SnakeAPI {
                 snake2 = addX(snake, new int[]{head2[0] - 1, head2[1]});
             }
         }
-        System.out.println(snake);
-        removeBack(snake);
+        if (board[head[0]][head[1]] != 1) {
+            snake = removeFront(snake);
+        }
+        System.out.println(Arrays.deepToString(snake));
         for (int i = 0; i < board.length;i++){
             for (int d = 0; d < board.length;d++){
                 boolean isInSnake = false;
@@ -90,7 +82,6 @@ public class SnakeAPI {
 
             }
         }
-        System.out.println(snake);
         return board;
     }
     public static void main(String[]args){
